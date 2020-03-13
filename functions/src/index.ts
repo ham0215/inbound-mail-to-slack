de0063a5edf5a46781aab7d0d5d2a2cd6a7d3531
naoto hamada <hamada@vis-its.com>
2020-03-13 11:53:41 +0900

    Add parameters type

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,19 +6,21 @@ const rp = require("request-promise");
 /**
  * Webhook that will be called each time received a new message in sendgrid and will post a message to Slack.
  */
-exports.sendgridWebhook = functions.https.onRequest(async (req, res) => {
-  console.log(req);
-  console.log(res);
-  try {
-    await postToSlack("hoge");
-    return res.end();
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .send("Something went wrong while posting the message to Slack.");
+exports.sendgridWebhook = functions.https.onRequest(
+  async (req: any, res: any) => {
+    console.log(req);
+    console.log(res);
+    try {
+      await postToSlack("hoge");
+      return res.end();
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .send("Something went wrong while posting the message to Slack.");
+    }
   }
-});
+);
 
 /**
  * Post a message to Slack about the new GitHub commit.
